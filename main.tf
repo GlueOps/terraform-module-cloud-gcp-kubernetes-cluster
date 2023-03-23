e165d2eec046d0f4209d939e8df75a0c33fd6a82
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-03-23 00:44:41 -0700

    feat: update gke channel (#25)

--- main.tf
+++ main.tf
@@ -178,7 +178,7 @@ resource "google_container_cluster" "gke" {
   name = "gke"
 
   location                    = var.zonal == true ? "${var.region}-a" : var.region
-  min_master_version          = "1.24.9-gke.1500"
+  min_master_version          = "1.24.9-gke.3200"
   remove_default_node_pool    = true
   initial_node_count          = 1
   enable_intranode_visibility = true
