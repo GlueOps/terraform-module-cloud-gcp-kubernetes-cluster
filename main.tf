59367b2d7fed31eec91cb27d07c8489dd1c66167
Venkata Mutyala <venkata@venkatamutyala.com>
2024-01-29 18:32:57 -0800

    feat: production readiness (#38)
    * feat: updates prod improvements
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
+      source = "hashicorp/google"
     }
   }
 }
@@ -117,14 +117,23 @@ resource "google_service_account" "gke_node_pool" {
   ]
 }
 
-resource "google_container_cluster" "gke" {
-  name = "gke"
+
+locals {
+  node_locations = [for node_location in var.cluster_supported_node_pool_zones : format("%s-%s", var.region, node_location)]
+}
+
+
+resource "google_container_cluster" "captain" {
+  name = "captain"
 
   location                    = var.zonal == true ? "${var.region}-a" : var.region
   min_master_version          = var.gke_version
   remove_default_node_pool    = true
   initial_node_count          = 1
   enable_intranode_visibility = true
+
+
+  node_locations = local.node_locations
 
   release_channel {
     channel = "UNSPECIFIED"
@@ -144,9 +153,37 @@ resource "google_container_cluster" "gke" {
     spot            = false
   }
 
+  monitoring_config {
+
+    managed_prometheus {
+      enabled = false
+    }
+    advanced_datapath_observability_config {
+      enable_metrics = false
+    }
+  }
+
   addons_config {
     horizontal_pod_autoscaling {
-      disabled = false
+      disabled = true
+    }
+    gcp_filestore_csi_driver_config {
+      enabled = false
+    }
+    gcs_fuse_csi_driver_config {
+      enabled = false
+
+    }
+    network_policy_config {
+      disabled = true
+
+    }
+
+    http_load_balancing {
+      disabled = true
+    }
+    gce_persistent_disk_csi_driver_config {
+      enabled = true
     }
   }
 
@@ -159,7 +196,10 @@ resource "google_container_cluster" "gke" {
   }
 }
 
-resource "google_container_node_pool" "custom_node_pool" {
+
+
+
+resource "google_container_node_pool" "custom_pools" {
   for_each = { for np in var.node_pools : np.name => np }
 
   network_config {
@@ -170,9 +210,10 @@ resource "google_container_node_pool" "custom_node_pool" {
   version = each.value.gke_version
 
   name    = each.value.name
-  cluster = google_container_cluster.gke.id
+  cluster = google_container_cluster.captain.id
 
   initial_node_count = each.value.node_count
+  node_locations     = [for node_location in each.value.node_pool_zones : format("%s-%s", var.region, node_location)]
 
   management {
     auto_upgrade = false
@@ -181,14 +222,23 @@ resource "google_container_node_pool" "custom_node_pool" {
 
   node_config {
     spot         = each.value.spot
+    preemptible = each.value.preemptible
     machine_type = each.value.machine_type
     disk_type    = each.value.disk_type
     disk_size_gb = each.value.disk_size_gb
 
     # If you still need the service account, add it as an input variable for the module
     service_account = google_service_account.gke_node_pool.email
+
+    labels = each.value.kubernetes_labels
+    dynamic "taint" {
+      for_each = each.value.kubernetes_taints
+      content {
+        effect = taint.value.effect
+        key    = taint.value.key
+        value  = taint.value.value
+      }
+    }
+
   }
 }
-
-
-
